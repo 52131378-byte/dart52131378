0eeeeae0950a1b8b43533f1a3c99dc7621822eca
52131378-byte <52131378@students.liu.edu.lb>
2025-11-24 14:11:16 +0200

    page 2

--- lib/main.dart
+++ lib/main.dart
@@ -51,9 +51,62 @@ if (selectedMood !=null)
 Text("Slelected :$selectedMood",
 style: const TextStyle(fontSize: 28),
 ),
-
-        ],
-      )
-    )
+            const Spacer(),
+            ElevatedButton(
+              onPressed: selectedMood == null
+                  ? null
+                  : () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PageTwo(mood: selectedMood!),
+                        ),
+                      );
+                    },
+              child: const Text("NEXT →"),
+            ),
+  Widget moodButton(String emoji) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedMood = emoji;
+        });
+      },
+      child: Text(
+        emoji,
+        style: const TextStyle(fontSize: 45),
+      ),
+    );
   }
 }
+ class pageTwo extends StatelessWidget {
+  final String mood ;
+  const PageTwo ({super.key , required this.mood});
+  @override 
+  widget build(BuildContext){
+    return Scaffold(
+      appBar : AppBar(
+        title: const Text("your Mood - Page 2"),
+        centerTitle: true,
+      ),
+      body: Padding(padding: const EdgeInsets.all(20)),
+      child : Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Your selected Mood is :",
+          style: TextStyle(fontSize: 22 , fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          Text(mood , style: const TextStyle(fontSize: 80),),
+          const SizedBox(height: 40),
+          ElevatedButton(onPressed: (){
+            navigator.pop(context);
+
+          }, child: const Text("Previous"),
+          )
+        ],
+      ),
+    ),
+    );
+  }
+ }
